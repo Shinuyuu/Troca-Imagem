f04c498f78afe8b65a025a690cbe1263730dc70c
= <=>
2023-11-30 21:41:20 -0300

    troca de maki

---
+++ moto.js
@@ -0,0 +1,4 @@
+img = document.getElementById('imagem')
+function clicou(){
+    img.setAttribute ('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShzN0Xh-B_efJF8VGlfff4tvD6IDYfxhRlvA&usqp=CAU')
+}
